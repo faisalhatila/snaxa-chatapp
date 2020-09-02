9f381755ffc2a4b8dd9f0b432220f856a59b8bfe
faisalhatila <hatilas1994@gmail.com>
2020-09-02 16:27:11 +0500

    chat main screen footer created

--- src/components/ChatMainScreenFooter.js
+++ src/components/ChatMainScreenFooter.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const ChatMainScreenFooter = (props) => {
-  return <div></div>;
+  return (
+    <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input
+        type="email"
+        class="form-control"
+        id="exampleInputEmail1"
+        aria-describedby="emailHelp"
+        placeholder="Enter email"
+      />
+      <small id="emailHelp" class="form-text text-muted">
+        We'll never share your email with anyone else.
+      </small>
+    </div>
+  );
 };
 
 export default ChatMainScreenFooter;
