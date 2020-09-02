0eb66bf7d58f48ed6fd17e976a5bf45c8b3e9f56
faisalhatila <hatilas1994@gmail.com>
2020-09-02 15:54:38 +0500

    chat component top done

--- src/components/ChatMainScreenTop.js
+++ src/components/ChatMainScreenTop.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const ChatScreenTopComponent = (props) => {
-  return <h1>hi</h1>;
+  return (
+    <div className="pt-4 pb-4">
+      <div>
+        <div className="d-flex align-items-center">
+          <span className="chatScreenTopActiveBulb mr-2"></span>
+          <h4 className="noMargin">Faisal</h4>
+        </div>
+        <div className="d-flex align-items-center">
+          <label className="noMargin smallFont11 heavyFont">Away |</label>
+          <label className="noMargin smallFont11 heavyFont">
+            &nbsp;Local time 2:20 PM
+          </label>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatScreenTopComponent;
